609311253c56a82627eabaf3cb5453b3aa160d83
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-09-26 18:16:39 -0500

    giphySearch.router set up, file structure set up, .env added, dependencies installed

---
+++ server/routes/giphySearch.router.js
@@ -0,0 +1,22 @@
+require("dotenv").config(); // env
+
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const giphy_api_key = process.env.GIPHY_API_KEY;
+
+router.get('/', (req, res) => {
+  let search = req.query.search;
+  axios
+    .get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${giphy_api_key}&limit=5`)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      console.log("GET /gifs fail: ", error);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App(props) {
+function App() {
+  const [theGifs, setTheGifs] = useState([]);
+
+  useEffect(() => {
+    fetchGifs();
+  }, []);
+
+  const search = "coding"
+
+
+  const fetchGifs = () => {
+    console.log("running fetchGifs");
+    axios
+      .get("/gifs", {
+        params: {
+          search: search,
+        },
+      })
+      .then((response) => {
+        const apiResponse = response.data;
+        console.log("API response: ", apiResponse.data);
+        setTheGifs(apiResponse.data);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
   return (
-    <div>
-      <h1>Giphy Search!</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">APIS</h1>
+        <h4>
+          <i>APIS</i>
+
+          {theGifs.map((gif) => {
+            return <img key={gif.id} src={gif.images.fixed_height.url} alt={gif.title} />;
+          })}
+        </h4>
+      </header>
+      <br />
     </div>
   );
 }
