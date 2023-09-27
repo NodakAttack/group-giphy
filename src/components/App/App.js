63fbc3c5f7e4bf88f0c6fb6e4d6b3ab6f28d047a
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-09-27 16:47:32 -0500

    saga/reducer set up but not working, error: reducer returning undefined

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const [theGifs, setTheGifs] = useState([]);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put } from "redux-saga/effects";
+
+// reducers
+const gifList = (state = [], action) => {
+    switch (action.type) {
+      case "FETCH_GIFS":
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+
+
+// sagas
+function* fetchGifs() {
+    try {
+      const gifsResponse = yield axios.get("/gifs");
+      yield put({ type: "FETCH_GIFS", payload: gifsResponse.data});
+    } catch (error) {
+      console.log("error fetching gifs", error);
+    }
+  }
+
+function* rootSaga() {
+  yield takeEvery("FETCH_GIFS", fetchGifs);
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  combineReducers({ gifList }),
+  applyMiddleware(sagaMiddleware, logger)
+);
+
+sagaMiddleware.run(rootSaga);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
